b70e4fd30b29a15149950aa0bd91f4c991952ed1
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:04:54 +0100

    Update 404.php

---
+++ view/404.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>404</title>
+    <link rel="stylesheet" href="view/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
+    <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.ico">
+</head>
+<body>
+    <div class="container">
+        <div class="error">
+            <h1 class="title_error">
+                <span>4</span>
+                <img class="img_error" src="images/favicon.png" alt="404">
+                <span>4</span>
+            </h1>
+            <p><br></p>
+            <p style="margin:0;">Oops! The page you are looking for does not exist or has been moved</p>
+            <a class="link" href="/home">Home</a>
+        </div>
+    </div>
