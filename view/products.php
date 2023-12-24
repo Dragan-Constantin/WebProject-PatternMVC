b128c6d063b1ff974b4314f4f36a3142cbfacf89
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:05:32 +0100

    Update products.php

--- view/products.php
+++ view/products.php
@@ -1,5 +1,19 @@
-<!-- Route « products » 
-•	Displays all the bikes (get them from `smartbike` bdd) with picture, price and a "add to cart" button
-•	The "cart" button displays whatever is in your cart, and the current sum total of your order (without tax or shipping fees)
-•	A button "More details" redirects to the page of the selected bike, meaning a route such as ("?page=produit&velo=1), which displays pictures of the bike, the price, a description and a "add to cart" button
-•	The « order » button redirects to the "/order" route
+<h1 style="text-align: center; font-size: 48px;">Our products</h1>
+<div class="product-container">
+    <?php foreach ($products as $product) {?>
+    <div class="product-card">
+        <img src="<?php echo $product['image_path']; ?>" alt="<?php echo $product['name']; ?>">
+        <hr>
+        <div class="product-info">
+            <h2><?php echo $product['name']; ?></h2>
+            <p>$<?php echo $product['price']; ?></p>
+        </div>
+        <hr>
+        <div class="buttons">
+            <a href="/products?id=<?php echo $product['id']; ?>" class="details-btn">More Details</a>
+            <a href="/order" class="order-btn">Order</a>
+        </div>
+    </div>
+    <?php }?>
+</div>
+<br><br>
