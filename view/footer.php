2e06b25dcb429c8c4959e2586187c9139bec0410
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:05:10 +0100

    Update footer.php

--- view/footer.php
+++ view/footer.php
@@ -1,6 +1,21 @@
-<footer>
-    <p>Copyright <?php echo $year, " ", $name;?> - All rights reserved.</p>
-</footer>
-
-</body>
+<footer class="footer">
+    <table>
+        <tr>
+            <td>
+                <p><strong>Copyright @<?php echo $year, " ", $name;?></strong></p>
+                <p>Email: <a href="mailto:contact@smartbike.com">contact@smartbike.com</a></p>
+                <p>Tel : <a href="tel:01-23-45-67-89">01-23-45-67-89</a></p>
+                <p>Adresse : <a href="https://www.google.fr/maps/place/Efrei/@48.7892876,2.3648773,18z/data=!4m10!1m2!2m1!1sefrei+parsi!3m6!1s0x47e673e24e04a9c3:0xc55cb3e676f95321!8m2!3d48.7887182!4d2.3637513!15sCgtlZnJlaSBwYXJpc5IBD3ByaXZhdGVfY29sbGVnZeABAA!16zL20vMDVtcHk0?entry=ttu">30-32 Avenue de la République 94800 Villejuif</a></p>
+            </td>
+            <td>
+                <div class="link_footer">
+                    <a href="/home">home</a>
+                    <a href="/products">products</a>
+                    <a href="/contact">contact</a>
+                    <a href="/order">order</a>
+                </div>
+            </td>
+        </tr>
+    </table>
+</footer>   
 </html>
