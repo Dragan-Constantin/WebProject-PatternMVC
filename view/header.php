f578c23a571969fe31c90783c72cb2f8897155a6
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:05:15 +0100

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,19 +1,27 @@
-<script>
-        function menuButton() {
-            var x = document.getElementById("myTopnav");
-            if (x.className === "topnav") {
-                x.className += " responsive";
-            } else {
-                x.className = "topnav";
-            }
-        }
-    </script>
+<!DOCTYPE html>
+
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartBike eShop</title>
+    <link rel="stylesheet" href="view/css/styles.css">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.png">
+    <link rel="shortcut icon" type="image/x-icon" href="./images/favicon.png">
 </head>
-<body>
-    <div class="topnav" id="myTopnav">
-        <div></div>
-        <a href="/contact">Contact</a>
-        <a href="/products">About</a>
-        <a href="/home">Home</a>
-        <a href="javascript:void(0);" class="icon" onclick="menuButton()"><i class="fa fa-bars"></i></a>
+<header>
+    <div class="logo">
+        <a href="/"><img src="images/smartbike.png" alt="Logo"></a>
     </div>
+    <nav>
+        <ul>
+            <li><a href="/">Home</a></li>
+            <li><a href="/products">Products</a></li>
+            <li><a href="/contact">Contact</a></li>
+            <li ><a class="link" href="/order">Order</a></li>
+        </ul>
+    </nav>
+</header>
+<body>
+</body>
+</html>
