9da5ce82d95193b90dc1c82f2c246122b5aa5a9d
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:05:01 +0100

    Create contact.php

---
+++ view/contact.php
@@ -0,0 +1,31 @@
+<div class="container">
+    <div class="card">
+        <span class="title">Leave a Comment</span>
+        <form class="form">
+            <div class="group">
+                <input placeholder="" type="text" required="">
+                <label for="name">Nom</label>
+            </div>
+            <div class="group">
+                <input placeholder="" type="text" required="">
+                <label for="name">Prénom</label>
+            </div>
+            <div class="group">
+                <input placeholder="" type="email" id="email" name="email" required="">
+                <label for="email">Email</label>
+            </div>
+            <div class="group">
+                <textarea placeholder="" id="comment" name="Message" rows="5" required=""></textarea>
+                <label for="comment">Message</label>
+            </div>
+            <button type="submit">Submit</button>
+        </form>
+    </div>
+    <div class="card">
+        <span class="title">Smartbike Information</span>
+        <iframe width="560" height="315" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=Ecole+Efrei&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed" frameborder="0" allowfullscreen></iframe>
+        <p>Email: <a href="mailto:contact@smartbike.com">contact@smartbike.com</a></p>
+        <p>Tel : <a href="tel:01-23-45-67-89">01-23-45-67-89</a></p>
+        <p>Adresse : <a href="https://www.google.fr/maps/place/Efrei/@48.7892876,2.3648773,18z/data=!4m10!1m2!2m1!1sefrei+parsi!3m6!1s0x47e673e24e04a9c3:0xc55cb3e676f95321!8m2!3d48.7887182!4d2.3637513!15sCgtlZnJlaSBwYXJpc5IBD3ByaXZhdGVfY29sbGVnZeABAA!16zL20vMDVtcHk0?entry=ttu">30-32 Avenue de la République 94800 Villejuif</a></p>
+    </div>
+</div>
