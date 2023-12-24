ef742187aff2d0b66747c6b6cad7e76b7572678f
Dragan Constantin <constantin.dragan@efrei.net>
2023-12-24 19:04:51 +0100

    Added url parameter support

--- router.php
+++ router.php
@@ -2,21 +2,44 @@
 
 function route_request($address){
     include_once("view/header.php");
-    switch ($address) {
-        case "/":
-            echo '<meta http-equiv="refresh" content="0;url=home">';
-            break;
-        case "/home":
-            break;
-        default:
-            include_once("view/404.php");
-            break;
+    global $productID;
+    
+    if ($address === "/") {
+        echo '<meta http-equiv="refresh" content="0;url=home">';
+    } elseif ($address === "/home") {
+        require_once("controller/homeController.php");
+        $page = new HomeController;
+        $page->getHomePage();
+        if (isset($_GET['id'])) {
+            $productID = $_GET['id'];
+        }
+    } elseif ($address === "/products") {
+        include_once("controller/productsController.php");
+        $page = new ProductsController;
+        $page->getProductsPage();
+    } elseif ($address === "/order") {
+        include_once("controller/orderController.php");
+        $page = new OrderController;
+        $page->getOrderPage();
+    } elseif ($address === "/contact") {
+        include_once("controller/contactController.php");
+        $page = new ContactController;
+        $page->getContactPage();
+    } elseif (strpos($address, "/products") === 0) {
+        $params = explode('?', $address);
+        if (isset($params[1]) && strpos($params[1], 'id=') === 0) {
+            $productID = substr($params[1], strpos($params[1], '=') + 1);
+            include_once("controller/itemController.php");
+            $page = new ItemController;
+            $page->getItemPage($productID);
+            // echo "<h1>This is the detail page of $productID</h1>";
+        }
+    } else {
+        include_once("view/404.php");
     }
     
-
-    // ! Footer keeps sticking at the bottom of screen instead of bottom of page.
-    // ! It blocks access to some buttons and iframes.
-    // TODO : Fix footer at the bottom of each page instead of bottom of screen
+    // var_dump($productID);
+    
     $year = date("Y");
     $name = "Smartbike";
     include_once("view/footer.php");
